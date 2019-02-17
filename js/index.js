3a7bca1b97c19a5806c3e8707448ec69d4fccb32
Dave Conroy <dconroy@gmail.com>
2019-02-16 20:37:25 -0700

    add api key

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 // TODO: Step 2: Setup Developer API Key
-
+let fm = new Fortmatic('pk_test_2886ED2326F7B9CE');
+web3 = new Web3(fm.getProvider());
 // End Step 2
 
 let handleSendTransaction = function(amount, address) {
-  // TODO: Step 3: Send Transaction Implementation
-
-  // End Step 3
+// TODO: Step 3: Send Transaction Implementation
+web3.eth.sendTransaction({
+  // From address will automatically be replaced by the address of current user
+  from: '0x0000000000000000000000000000000000000000',
+  to: address,
+  value: web3.utils.toWei(amount, 'ether')
+});
+// End Step 3
 };
 
 
